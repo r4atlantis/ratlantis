2a06209cb18c9f3165b8d417d9cc1d20c9b1a892
J. Stephen Gosnell <stephen.gosnell@baruch.cuny.edu>
2015-02-22 21:46:58 -0500

    *added fake function with roxygen2 documentation to check setup

---
+++ R/test_cat_function.R
@@ -0,0 +1,19 @@
+#' A Cat Function
+#'
+#' This function allows you to express your love of cats.
+#' @param love Do you love cats? Defaults to TRUE.
+#' @keywords cats
+#' @export
+#' @examples
+#' cat_function()
+
+cat_function <- function(love=TRUE){
+  if(love==TRUE){
+    print("I love cats!")
+  }
+  else {
+    print("I am not a cool person.")
+  }
+}
+
+#from http://hilaryparker.com/2014/04/29/writing-an-r-package-from-scratch/
