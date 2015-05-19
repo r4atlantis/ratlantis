5c03a226590aec6a2a5684c31b2e394a912b56ad
J. Stephen Gosnell <stephen.gosnell@baruch.cuny.edu>
2015-05-19 10:16:33 -0400

    Updated name of helper function to fix warning

--- R/helper_functions_ratlantis.R
+++ R/helper_functions_ratlantis.R
@@ -1,11 +1,14 @@
 #helper functions for ratlantis
 
-#' "Not in" function
+#' "not in" function
 #'
 #' This function allows you to check if an element is not in a larger group
+#' @name not_in
+#' @aliases not_in
 #' @param x object you are trying to check for
 #' @param table group you are checking against
 #' @export
 #define not in function
-"%!in%" <- function(x,table) match(x,table, nomatch = 0) == 0
+not_in <- function(x,table) match(x,table, nomatch = 0) == 0
+"%!in%" <- not_in
 
