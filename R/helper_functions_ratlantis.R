a4f67bd02e5be68054f73002ce9affefb2492c83
J. Stephen Gosnell <stephen.gosnell@baruch.cuny.edu>
2015-05-19 10:37:11 -0400

    updated travis with code to load gdal libraries (https://github.com/ropensci/rWBclimate/blob/master/.travis.yml); removed rgeos from imports

--- R/helper_functions_ratlantis.R
+++ R/helper_functions_ratlantis.R
@@ -1,6 +1,6 @@
 #helper functions for ratlantis
 
-#' "not in" function
+#' not_in function
 #'
 #' This function allows you to check if an element is not in a larger group
 #' @name not_in
@@ -8,6 +8,7 @@
 #' @param x object you are trying to check for
 #' @param table group you are checking against
 #' @export
+#' @details Also allows uses of "%!in%"
 #define not in function
 not_in <- function(x,table) match(x,table, nomatch = 0) == 0
 "%!in%" <- not_in
