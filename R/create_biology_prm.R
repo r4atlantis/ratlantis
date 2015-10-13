d6a8bb379ff000baeae20ec1bd9a6ebf500ce776
J. Stephen Gosnell <stephen.gosnell@baruch.cuny.edu>
2015-10-13 14:51:59 -0400

    updated documention  and to do list

--- R/create_biology_prm.R
+++ R/create_biology_prm.R
@@ -1,8 +1,7 @@
 #' create_biology_prm function
 #'
 #' This function creates the biology prm file needed for Atlantis
-#' @param map_name name of map to be used for bgm creation. Must incude "box_id"
-#' column. Other columns may be included (horizmix, vertmix,boundary_code)
+#' @param
 #' @keywords biology prm
 #' @details This function creates the biology prm file needed by Atlantis. User
 #' create functional groupings using the create_functional_groups function or supply
