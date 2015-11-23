bcbbf97cb77337e7c0852c2b18693ff56a88820f
J. Stephen Gosnell <stephen.gosnell@baruch.cuny.edu>
2015-11-23 14:20:13 -0500

    update execute file to handle optional arguments

--- R/execute_atlantis.R
+++ R/execute_atlantis.R
@@ -22,6 +22,8 @@
 #' location as the atlantis exe file)
 #' @param fisheries_csv list delimiting targeted fisheries (needs to be in same
 #' location as the atlantis exe file)
+#' @param econ_prm name of econ file (needs to be in same location as the
+#' atlantis exe file)
 #' @param output_folder, name of folder (in same directory as atlantis.exe file)
 #' where should results be stored?
 #' @keywords atlantis
@@ -32,12 +34,24 @@
 execute_atlantis <- function (atlantis_location, atlantis_exe, biology_nc, output_file_nc,
                               run_file_prm, forcing_time_series_prm,physics_prm,
                               biology_prm, harvest_prm, functional_group_csv,
-                              fisheries_csv, output_folder
+                              fisheries_csv, econ_prm, output_folder
                               ){
 
-  shell(paste("cd ", atlantis_location, " && ", atlantis_exe, " -i ", biology_nc," 0 -o ",
-              output_file_nc, " -r ", run_file_prm, " -f ", forcing_time_series_prm, " -p ",
-              physics_prm, " -b ", biology_prm, " -h  ", harvest_prm, " -s ", functional_group_csv,
-              " -q ", fisheries_csv, " ", " -d ", output_folder, sep=""), intern=T)
+  #first do required commands
+
+  command_argument <- paste("cd ", atlantis_location, " && ", atlantis_exe, " -i ", biology_nc," 0 -o ",
+                            output_file_nc, " -r ", run_file_prm, " -f ", forcing_time_series_prm, " -p ",
+                            physics_prm, " -b ", biology_prm, " -h  ", harvest_prm,
+                            " -s ", functional_group_csv,
+                            " -q ", fisheries_csv, " ", " -d ", output_folder,
+                            sep = "")
+
+  #for optional parameters
+
+  if(missing(econ_prm) == F){
+    command_argument <- paste(command_argument, " -e ", econ_prm, sep = "")
+  }
+
+  shell(command_argument, intern = T)
 
 }
