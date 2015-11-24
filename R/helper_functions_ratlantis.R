293a88ee851fb8cf3a1815a279350cc4324e0d51
J. Stephen Gosnell <stephen.gosnell@baruch.cuny.edu>
2015-11-24 01:04:09 -0500

    more "%!in%" fixes

--- R/helper_functions_ratlantis.R
+++ R/helper_functions_ratlantis.R
@@ -5,7 +5,7 @@
 #' This function allows you to check if an element is not in a larger group
 #' @param x object you are trying to check for
 #' @param table group you are checking against
-"%!in%" <- function(x,table) match(x,table, nomatch = 0) ==
+"%!in%" <- function(x,table) match(x,table, nomatch = 0) == 0
 
 
 #' meannona function
@@ -15,7 +15,6 @@
 #' @param ... list for which you seeking a mean
 meannona=function(...){
   mean(..., na.rm=T)
-
 }
 
 #' maxnona function
